f95a033c793ab7cb193e8afabab2b3e44f5a9c6e
Meunier A. <123185224+Meunier6969@users.noreply.github.com>
2024-10-06 01:13:49 -0400

    almost finished

--- PlusOuMoins/main.c
+++ PlusOuMoins/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <time.h>	// Random number
 #include <stdlib.h>
 
 // Get a random number between lb and hb (inclusive)
-uint16_t getRandomNumber(uint16_t lb, uint16_t hb)
+int getRandomNumber(int lb, int hb)
 {
 	return lb + (rand() % (hb - lb + 1));
 }
@@ -13,13 +12,26 @@ int main()
 {
 	srand(time(NULL)); // Initialization
 
-	uint16_t lowerBound = 0;
-	uint16_t higherBound = 100;
+	int choice;
 
-	uint8_t try = 0;
+	int lowerBound, higherBound;
 
-	uint16_t guess;
-	uint16_t randomNumber = getRandomNumber(lowerBound, higherBound);
+	int try;
+
+	int guess;
+	int randomNumber;
+	
+	BEGINNING:
+
+	printf("Bienvenue de ce jeu de Plus ou Moins !");
+
+	printf("Enter la borne minimale : ");
+	scanf("%d", &lowerBound);
+	printf("Enter la borne maximale : ");
+	scanf("%d", &higherBound);
+	
+	try = 0;
+	randomNumber = getRandomNumber(lowerBound, higherBound);
 
 	do
 	{
@@ -33,16 +45,17 @@ int main()
 			printf("Le nombre est plus grand\n");
 
 	} while (guess != randomNumber);
-	
+
 	// winner
 	if (try == 1)
-	{
 		printf("Tu as trouvé.e le nombre du premier coup!\n");
-	} 
 	else
-	{
 		printf("Tu as trouvé.e le nombre en %d essais!\n", try);
-	}
+
+	printf("Veux tu rejouer ? (y/N) ");
+	scanf(" %c", &choice);
+	if (choice == 'y')
+		goto BEGINNING;
 
 	return 0;
 }

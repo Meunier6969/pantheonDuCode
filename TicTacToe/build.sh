b4e4f415917f522e72779f1ab6222c807f8fc773
Meunier A. <123185224+Meunier6969@users.noreply.github.com>
2024-10-24 01:42:50 -0400

    ééé

---
+++ TicTacToe/build.sh
@@ -0,0 +1,2 @@
+nasm -f elf64 source.asm
+ld -melf_x86_64 -o a.out source.o
--- TicTacToe/source.asm
+++ TicTacToe/source.asm
@@ -1,5 +1,5 @@
 section .data
-	num db 69
+	
 
 section .bss
     buffer resb 16
@@ -8,11 +8,14 @@ section .text
 	global _start
 
 _start:
-    mov eax, 4    ; sys_write
-    mov ebx, 1    ; 1 => stdout
-    mov ecx, [num] ; msg
-    mov edx, 4    ; length of output
-	int 0x80       ; interupt
+    push    '!'         ; push qword 0x21
+	mov     rax, 1      ; write call number, __NR_write from unistd_64.h
+	mov     edi, 1      ; write to stdout (int fd=1)
+	mov     rsi, rsp    ; use char on stack
+	mov     rdx, 1      ; size_t len = 1 char to write.
+	syscall            ; call the kernel, it looks at registers to decide what to do
+	add     rsp, 8      ; restore stack pointer
+
 
 	mov eax, 1     ; sys_exit
 	mov ebx, 0     ; return code
